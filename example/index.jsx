ae9b9b1d45cd7d0ba438d45dfe64cff73e78f904
wangyan <wyvernnot@gmail.com>
2016-01-19 13:34:35 +0800

    Redux example

--- example/index.jsx
+++ example/index.jsx
@@ -3,11 +3,48 @@
  */
 
 import React from 'react';
-import Control from '../Control.jsx';
+import Control from '../redux/Control.jsx';
 import {render} from 'react-dom';
 
-var elem = document.createElement('div');
-render(<Control/>, elem);
+import {createStore} from 'redux';
+import {Provider,connect} from 'react-redux';
+
+const reducer = (state = {
+  from_ts: Date.now() - 360000,
+  to_ts: Date.now()
+}, action)=> {
+  switch (action.type) {
+    case "SET_FROM_TS":
+      return Object.assign({}, state, {
+        from_ts: action.ts
+      });
+    case "SET_TO_TS":
+      return Object.assign({}, state, {
+        to_ts: action.ts
+      });
+    default:
+      return state;
+  }
+}
+
+let elem = document.createElement('div');
+let store = createStore(reducer);
+
+let APP = (props)=> {
+  return <pre>props:{JSON.stringify(props, null, 2)}</pre>
+}
+
+APP = connect(state=>({
+  from_ts: state.from_ts,
+  to_ts: state.to_ts,
+}))(APP)
+
+render(<Provider store={store}>
+  <div>
+    <Control/>
+    <APP/>
+  </div>
+</Provider>, elem);
 
 document.body.appendChild(elem);
 
